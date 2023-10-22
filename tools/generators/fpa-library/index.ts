460ea4a517472e185ab9163585390e8e60c6ecd6
Frank-Peter Andrä <info@frankpeterandrae.de>
2023-10-22 03:28:59 +0200

    add first config

---
+++ tools/generators/fpa-library/index.ts
@@ -0,0 +1,7 @@
+import { Tree } from '@nrwl/devkit'
+import { AppSchema } from './schema'
+
+export default async function (tree: Tree, options: AppSchema){
+
+}
+
---
+++ tools/generators/fpa-library/schema.d.ts
@@ -0,0 +1,7 @@
+export interface AppSchema {
+  name: string;
+  directory?: string;
+  feature?: boolean;
+  onlyConfig?: boolean;
+  tags?: string;
+}
